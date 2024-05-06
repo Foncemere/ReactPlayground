8ef757399e9f25034b80acc5cd9e3b82ed1f4f45
Foncemere <karltaguibao@gmail.com>
2024-05-06 13:38:09 +0800

    Styling test 2, able to use TailwindCSS after modifying tailwind.config.ts

--- src/HelloWorldComponent/index.tsx
+++ src/HelloWorldComponent/index.tsx
@@ -2,7 +2,7 @@ export const HelloWorldComponent = (props) => {
   return (
     <div
       className="p-10 border-2 rounded border-black"
-      style={{ padding: 10, borderColor: "red", borderWidth: 1 }}
+      // style={{ padding: 10, borderColor: "red", borderWidth: 1 }}
     >
       <p>Hiii World</p>
     </div>
