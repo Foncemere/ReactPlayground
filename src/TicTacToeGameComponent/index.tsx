690b72e55616f19c17283c705b57cc9f769e80d8
Foncemere <karltaguibao@gmail.com>
2024-05-06 16:49:41 +0800

    Added working squares

--- src/TicTacToeGameComponent/index.tsx
+++ src/TicTacToeGameComponent/index.tsx
@@ -2,26 +2,30 @@
 import { useState } from "react";
 
 export const TicTacToeGameComponent = (props) => {
-  const [currPlayer, setCurrPlayer] = useState(0);
+  const [currPlayer, setCurrPlayer] = useState(false);
   const [board, setBoard] = useState({});
   //object to know who claims the square they are on, when this is updated
   // we will call a useEffect to see if there's a winner.
+  const claimSquare = (id) => {
+    setBoard({ ...board, [id]: currPlayer });
+    setCurrPlayer(!currPlayer);
+  };
   return (
     <div>
       <div style={{ flexDirection: "row", display: "flex" }}>
-        <Square id={0} />
-        <Square id={1} />
-        <Square id={2} />
+        <Square id={0} currentPlayer={board[0]} onClick={claimSquare} />
+        <Square id={1} currentPlayer={board[1]} onClick={claimSquare} />
+        <Square id={2} currentPlayer={board[2]} onClick={claimSquare} />
       </div>
       <div style={{ flexDirection: "row", display: "flex" }}>
-        <Square id={3} />
-        <Square id={4} />
-        <Square id={5} />
+        <Square id={3} currentPlayer={board[3]} onClick={claimSquare} />
+        <Square id={4} currentPlayer={board[4]} onClick={claimSquare} />
+        <Square id={5} currentPlayer={board[5]} onClick={claimSquare} />
       </div>
       <div style={{ flexDirection: "row", display: "flex" }}>
-        <Square id={6} />
-        <Square id={7} />
-        <Square id={8} />
+        <Square id={6} currentPlayer={board[6]} onClick={claimSquare} />
+        <Square id={7} currentPlayer={board[7]} onClick={claimSquare} />
+        <Square id={8} currentPlayer={board[8]} onClick={claimSquare} />
       </div>
     </div>
   );
@@ -30,11 +34,24 @@ export const TicTacToeGameComponent = (props) => {
 const Square = (props) => {
   return (
     <button
+      style={{
+        height: 36,
+        width: 36,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
       id={props.id}
-      onClick={() => {}}
+      onClick={() => props.onClick(props.id)}
       className={"p-5 border-2 border-black"}
     >
-      <p>{"x"}</p>
+      <p>
+        {props.currentPlayer === false
+          ? "x"
+          : props.currentPlayer === true
+            ? "o"
+            : null}
+      </p>
     </button>
   );
 };
