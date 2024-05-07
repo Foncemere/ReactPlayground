4c66a05cdf697da9c5e07d1cb93e51e491e020df
Foncemere <karltaguibao@gmail.com>
2024-05-07 19:33:57 +0800

    Updated to have a simple socket

--- src/TicTacToeGameComponent/index.tsx
+++ src/TicTacToeGameComponent/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import { socket } from "./socket";
 
 const winningCombos = [
   // horizontal
@@ -46,6 +47,24 @@ export const TicTacToeGameComponent = (props) => {
     setWinner(null);
     setCurrPlayer(false);
   };
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+    function onConnect() {
+      console.log("this is connected");
+    }
+    function onDisconnect() {
+      console.log("this is disconnected");
+    }
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
 
   useEffect(() => {
     //there can be a winner after 5 rounds (starter will have at least 3 squares)
